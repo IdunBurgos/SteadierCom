1aba9dd6c5c0c7a31afff22fc2d53180c85e0f94
IdunBurgos <idun.m.t.burgos@ntnu.no>
2025-04-14 12:50:16 +0200

    Changed abstol, added support for agora reactions, improved efficiency processing of SteadierCom results

--- src/steadiercom/cli.py
+++ src/steadiercom/cli.py
@@ -12,6 +12,7 @@ from reframed.io.cache import ModelCache
 import os
 from reframed import Environment, set_default_solver
 from math import inf
+import time # test time
 
 
 def extract_id_from_filepath(filepath):
@@ -27,20 +28,21 @@ def extract_id_from_filepath(filepath):
     return organism_id
 
 
-def build_cache(models):
+def build_cache(models,exchange_detection=None): # NEW
     ids = [extract_id_from_filepath(model) for model in models]
 
-    return ModelCache(ids, models, load_args={'flavor': 'bigg'})
+    return ModelCache(ids, models, load_args={'flavor': 'bigg','exchange_detection':exchange_detection}) # Change here? add extra argument for regex
+        
 
 
-def load_communities(models, communities):
+def load_communities(models, communities,exchange_detection=None): ## change here? add extra argument for regex
     if len(models) == 1 and '*' in models[0]:
         pattern = models[0]
         models = glob.glob(pattern)
         if len(models) == 0:
-            raise RuntimeError(f'No files found: {pattern}')
+            raise RuntimeError(f'No files found: {pattern}') ## Change here? add extra argument for regex
         
-    model_cache = build_cache(models)
+    model_cache = build_cache(models,exchange_detection)
 
     has_abundance = False
 
@@ -69,22 +71,55 @@ def load_media_db(filename):
 
     media_db = {}
     for medium, data_i in data.groupby('medium'):
+
         if has_bounds:
-            media_db[medium] = dict(data_i[['compound', 'bound']].values)
+            media_db[medium] = dict(data_i[['compound', 'bound']].values) 
         else:
             media_db[medium] = list(data_i['compound'])
 
     return media_db, has_bounds
 
 
+
+def precompute_exchange_map(community):
+    ## NEW FUNCTION
+    exchange_map = {}
+    community.merge_models()
+    reaction_map = community.reaction_map
+    external_metabolites = list(community.merged_model.get_external_metabolites())
+
+    for org_id, organism in community.organisms.items():
+        for m_id in external_metabolites:
+            if m_id not in organism.metabolites:
+                continue
+
+            rxn_list = []
+            for r_id in organism.get_metabolite_reactions(m_id):
+                if (org_id, r_id) in reaction_map:
+                    rxn_index = reaction_map[(org_id, r_id)]
+                    coeff = organism.reactions[r_id].stoichiometry.get(m_id, 0)
+                    rxn_list.append((rxn_index, coeff))
+
+            if rxn_list:
+                exchange_map[(org_id, m_id)] = rxn_list
+
+    return external_metabolites, exchange_map
+
+
 def main_run(models, communities=None, output=None, media=None, mediadb=None, growth=None, sample=None, 
-             w_e=None, w_r=None, target=None, unlimited=None):
+             w_e=0.002, w_r=0.2, target=None, unlimited=None,exchange_detection=None): # Added exchange_detection
 
-    abstol = 1e-9
+    abstol = 1e-6
     default_growth = 0.1
-
-    model_cache, comm_dict, has_abundance = load_communities(models, communities)
-
+    
+    start = time.time()
+    
+    
+    model_cache, comm_dict, has_abundance = load_communities(models, communities,exchange_detection) ## NEW Add extra argument for regex for exchange reactions
+    
+    time1 = time.time()
+    print(f"loading communities took: {time1 - start:.4f} seconds")
+    
     if media is None:
         media = [None]
     else:
@@ -104,18 +139,24 @@ def main_run(models, communities=None, output=None, media=None, mediadb=None, gr
     
     if not has_abundance and growth is None:
         growth = default_growth
-
+    
+    
+    time2 = time.time()
     for comm_id, organisms in comm_dict.items():
-
+        
         if has_abundance:
             abundance = organisms
             organisms = organisms.keys()
         else:
             abundance = None
+            
 
         comm_models = [model_cache.get_model(org_id, reset_id=True) for org_id in organisms]
         community = Community(comm_id, comm_models, copy_models=False)
-
+        
+        
+        external_metabolites, exchange_map = precompute_exchange_map(community)
+        
         if target is not None and target not in community.merged_model.reactions:
             raise RuntimeError(f'Invalid target reaction: {target}')
 
@@ -125,11 +166,20 @@ def main_run(models, communities=None, output=None, media=None, mediadb=None, gr
                 medium = 'complete'
                 env = Environment.complete(community.merged_model, inplace=False)
             else:
-                env = Environment.from_compounds(media_db[medium]).apply(community.merged_model, inplace=False, exclusive=True, warning=False)
+                # ADD make media from compounds/reactions - NB assumes that entries have the same prefix
+                string_type = media_db[list(media_db.keys())[0]][0]
+                if string_type.startswith("R_"):
+                    env = Environment.from_reactions(media_db[medium]).apply(community.merged_model, inplace=False, exclusive=True, warning=False)
+                else:
+                    env = Environment.from_compounds(media_db[medium]).apply(community.merged_model, inplace=False, exclusive=True, warning=False)
 
                 if media_has_bounds:
                     for cpd, bound in media_db[medium].items():
-                        r_id = f'R_EX_{cpd}_e'
+                        # ADD - if cpd already has R_ prefix - don't add anything!
+                        if cpd.startswith("R_"):
+                            r_id = cpd
+                        else:
+                            r_id = f'R_EX_{cpd}_e'
                         if r_id in env:
                             env[r_id] = (-bound, inf)
 
@@ -139,36 +189,45 @@ def main_run(models, communities=None, output=None, media=None, mediadb=None, gr
                 env.update(Environment.from_compounds(unlimited, max_uptake=1000).apply(community.merged_model, inplace=False, exclusive=False, warning=False))
                 
             if sample is None:
-                sol = SteadierCom(community, abundance=abundance, growth=growth, allocation=True, constraints=env, w_e=w_e, w_r=w_r, objective=target)
+                sol = SteadierCom(community,abundance=abundance, growth=growth, allocation=True, constraints=env, w_e=w_e, w_r=w_r, objective=target)
                 if sol.status == Status.OPTIMAL:
-                    df = sol.cross_feeding(as_df=True).fillna('environment')
+                    df = sol.cross_feeding(external_metabolites=external_metabolites, exchange_map=exchange_map,as_df=True).fillna('environment')
                     df['community'] = comm_id
                     df['medium'] = medium
                     results.append(df)
             else:
                 sols = SteadierSample(community, n=sample, abundance=abundance, growth=growth, allocation=True, constraints=env, w_e=w_e, w_r=w_r, objective=target)
-                feasible = [sol.cross_feeding(as_df=True).fillna('environment') for sol in sols if sol.status == Status.OPTIMAL]
+
+                feasible = [sol.cross_feeding(external_metabolites=external_metabolites, exchange_map=exchange_map,as_df=True).fillna('environment') for sol in sols if sol.status == Status.OPTIMAL]
+
+
                 if len(feasible) > 0:
-                    df = pd.concat(feasible)
+                    df = pd.concat(feasible,ignore_index=True)
                     df['frequency'] = 1
                     df = df.groupby(['donor', 'receiver', 'compound'], as_index=False).agg(
                         {'mass_rate': lambda x: x.mean(), 'rate': lambda x: x.mean(), 'frequency': lambda x: sum(x)/len(feasible)})
                     df['community'] = comm_id
                     df['medium'] = medium
                     results.append(df)
-
+    time3 = time.time()
+    print(f"Running SteadierCom and gathering data took: {(time3-time2)/60:.2f} minutes")
+    
     if not output:
         output_file = 'output.tsv'
     else:
         output_file = f'{output}.tsv'
 
     if len(results) > 0:
-        df_all = pd.concat(results).query(f'rate > {abstol}').sort_values(['community', 'medium', 'mass_rate'], ascending=False)
+        df_all = pd.concat(results,ignore_index=True).query(f'rate > {abstol}').sort_values(['community', 'medium', 'mass_rate'], ascending=False) #added ignore_index=True to concat
 
         if unlimited is not None:
             df_all = df_all[~df_all['compound'].isin(unlimited_ids)]
 
         df_all.to_csv(output_file, sep='\t', index=False)
+        
+        time4 = time.time()
+        print(f"Further processing took: {(time4-time3)/60:.2f} minutes")
+        
         return df_all
     else:
         print('No feasible solutions found.')
@@ -223,6 +282,7 @@ def main():
     parser.add_argument('--target', help="Target reaction to maximize (optional)")
     parser.add_argument('--solver', help="Select LP solver (options: gurobi, cplex, scip)")
     parser.add_argument('--unlimited', help="Compounds to be considered in excess supply (excluded from cross-feeding analysis).")
+    parser.add_argument('--exchange-detection', help="Type of exchange detection 'unbalanced'/'boundary' OR regular expression for exchange reactions") # NEW
 
     args = parser.parse_args()
 
@@ -241,6 +301,7 @@ def main():
         w_r = args.wr,
         target = args.target,
         unlimited=args.unlimited,
+        exchange_detection = args.exchange_detection, #NEW
     )
 
 
